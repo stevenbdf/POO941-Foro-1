15c41327eb0a1f5583506b9949156532a30a56ab
Steven Diaz <stevenbdf@gmail.com>
2021-02-07 23:48:43 -0600

    📝 Add documentation

--- src/estudiantes/Estudiantes.java
+++ src/estudiantes/Estudiantes.java
@@ -8,13 +8,11 @@ package estudiantes;
 import java.util.Scanner;
 import java.util.HashMap;
 
-/**
- *
- * @author elaniin
- */
 public class Estudiantes {
 
+    // Static Scanner instance for the entire class
     static Scanner scanner = new Scanner(System.in);
+    // Static HasMap to have access to students in the entire class
     static HashMap<String, String> students = new HashMap<String, String>();
 
     /**
@@ -25,6 +23,7 @@ public class Estudiantes {
         boolean exitProgram = true;
 
         do {
+            // Get selected option
             int optionMenu = printMenu();
 
             switch (optionMenu) {
@@ -41,11 +40,14 @@ public class Estudiantes {
                     getStudents();
                     break;
                 case 5:
+                    // Exit program 
                     exitProgram = true;
                     break;
             }
 
+            // If option is different from 5 show runAgain messages
             if (optionMenu != 5) {
+                // Negate returned value to exit do while
                 exitProgram = !runAgain();
             }
 
@@ -62,6 +64,7 @@ public class Estudiantes {
         System.out.println("\nIngresa el nombre del alumno");
         String studentName = scanner.nextLine();
 
+        // Add record to students HashMap
         students.put(studentId, studentName);
 
         System.out.println("\nAlumno ingresado exitosamente");
@@ -74,6 +77,7 @@ public class Estudiantes {
         System.out.println("\nIngresa el carnet del alumno a buscar");
         String studentId = scanner.nextLine();
 
+        // Try to get student name, if doesn't exists show default message
         System.out.println("\n" + students.getOrDefault(studentId, "Alumno no encontrado, no se puede mostrar"));
     }
 
@@ -83,7 +87,11 @@ public class Estudiantes {
     public static void deleteStudent() {
         System.out.println("\nIngresa el carnet del alumno a eliminar");
         String studentId = scanner.nextLine();
-        
+
+        /**
+         * remove returns deleted key, if deleted key is different from null
+         * show success message, else show failure message
+         */
         if (students.remove(studentId) != null) {
             System.out.println("\nAlumno eliminado exitosamente");
         } else {
@@ -96,15 +104,22 @@ public class Estudiantes {
      */
     public static void getStudents() {
         System.out.println("");
-        students.forEach((studentId, studentName) -> System.out.println(studentId + " - " + studentName ));
+        
+        /**
+         * Iterate students with forEach Lambda Expression
+         * 
+         * forEach receives key, value -> action
+         */
+        students.forEach((studentId, studentName) -> System.out.println(studentId + " - " + studentName));
     }
-    
+
     /**
      * @return the selected option in the menu
      */
     public static int printMenu() {
         boolean isOptionValid = false;
 
+        // default return option is exit program
         int returnOption = 5;
 
         do {
@@ -123,11 +138,14 @@ public class Estudiantes {
             int option = scanner.nextInt();
             scanner.nextLine();
 
+            // if option has one of these values, option is valid
             isOptionValid = option == 1 || option == 2 || option == 3 || option == 4 || option == 5;
 
             if (isOptionValid) {
+                // if option is valid set return option to option
                 returnOption = option;
             } else {
+                // else show invalid message and try again
                 System.out.println("Opcion invalida intentalo de nuevo");
             }
 
@@ -150,11 +168,14 @@ public class Estudiantes {
             int option = scanner.nextInt();
             scanner.nextLine();
 
+            // if option is one of these values, option is valid
             isOptionValid = option == 1 || option == 2;
 
             if (isOptionValid) {
+                // if option is 1 set runAgain = true, else runAgain = false
                 runAgain = option == 1;
             } else {
+                // else show invalid message and try again
                 System.out.println("Opcion invalida intentalo de nuevo");
             }
 
